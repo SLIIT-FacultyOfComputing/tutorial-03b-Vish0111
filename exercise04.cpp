d175a842639b1f5b90bbc9553dcc99bda09c41d2
IT21315282 Vishwa Udayantha <94515855+Vish0111@users.noreply.github.com>
2022-08-17 04:43:35 +0000

    Tutorial 3

--- exercise04.cpp
+++ exercise04.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void print(int len, int wth);
 void input(int len, int wth);
+void input(int length, int width);
 
 // Do not change the main() function
 int main() {
@@ -19,3 +20,7 @@ void print(int len, int wth) {
 }
 
 // Implement the Input Function here
+void input(int &length, int &width)
+{
+  return ;
+}
